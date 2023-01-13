e7a80b6bd3fe8be5d89e77a77ffbbb50a9e6deb1
kana_xedge <96711095+kanakiku@users.noreply.github.com>
2023-01-13 22:57:09 +0900

    202301132257

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,6 +30,17 @@
     <p>
       testtext
     </p>
+      <p>count</p><input type="number" max="1000" min="10" :value="top" @change="bottom">
+      <!--<br>
+      <p>分母</p><input type="number" max="1000" min="10" :value="bottom" @change="top">-->
+      <br>
+      <div class="relative">
+        <img id="gazo" class="coffetree" src="https://drive.google.com/uc?export=view&id=1K2j0JCF9CppLWob6YvZYvJR-ROm0NEIJ" width="540" height="960">
+        <img id = "beans1" src="https://drive.google.com/uc?export=view&id=1IR24pMQ6-A1cgiSEhIIR7p5TwPnqr5no" class="absolute">
+        <div id="app">
+          <canvas id="canvas" width="500" height="250"></canvas>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -58,4 +69,13 @@ li {
 a {
   color: #42b983;
 }
+.relative {
+  position: relative;
+}
+.absolute {
+  position: absolute;
+  right: 1000px;
+  bottom: 1000px;
+}
+
 </style>
