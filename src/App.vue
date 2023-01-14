4938066c75f35e98a698c856d327eb63a2efb883
kana_xedge <96711095+kanakiku@users.noreply.github.com>
2023-01-15 01:27:04 +0900

    202301150126

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <div id="mycanvas">
+          <canvas id="canvas" width="500" height="250"></canvas>
+  </div>
+  <br>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -11,6 +15,8 @@ export default {
   components: {
     HelloWorld
   }
+
+  
 }
 </script>
 
@@ -22,5 +28,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#mycanvas {
+    width: 100%;
+    max-width: 500px;
+    margin: 40px auto;
+    padding: 28px;
+    border: 1px solid #ffffff;
+    border-radius: 5px;
+    background-color: transparent;
+    box-shadow: 2px 2px 5px 0px rgba(200, 200, 200, 1);
+    font-family: "Times New Roman";
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,11 +6,6 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
     <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
@@ -19,27 +14,20 @@
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
     <p>
       testtext
     </p>
       <p>count</p><input type="number" max="1000" min="10" :value="top" @change="bottom">
+      <br>
+      <div id="app">
+          <canvas id="canvas" width="500" height="250"></canvas>
+      </div>
       <!--<br>
       <p>分母</p><input type="number" max="1000" min="10" :value="bottom" @change="top">-->
       <br>
       <div class="relative">
         <img id="gazo" class="coffetree" src="https://drive.google.com/uc?export=view&id=1K2j0JCF9CppLWob6YvZYvJR-ROm0NEIJ" width="540" height="960">
         <img id = "beans1" src="https://drive.google.com/uc?export=view&id=1IR24pMQ6-A1cgiSEhIIR7p5TwPnqr5no" class="absolute">
-        <div id="app">
-          <canvas id="canvas" width="500" height="250"></canvas>
-        </div>
       </div>
   </div>
 </template>
@@ -49,7 +37,8 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+
 }
 </script>
 
@@ -76,6 +65,21 @@ a {
   position: absolute;
   right: 1000px;
   bottom: 1000px;
+}
+#app {
+    width: 96%;
+    max-width: 500px;
+    margin: 40px auto;
+    padding: 28px;
+    border: 1px solid #ffffff;
+    border-radius: 5px;
+    background-color: transparent;
+    box-shadow: 2px 2px 5px 0px rgba(200, 200, 200, 1);
+    font-family: "Times New Roman";
+}
+
+#canvas {
+    margin: 0 auto;
 }
 
 </style>
