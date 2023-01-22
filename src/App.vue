4e0c675702dc3eae0031819d47e907fef3956048
kana_xedge <96711095+kanakiku@users.noreply.github.com>
2023-01-22 15:57:06 +0900

    202301221557

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div id="mycanvas">
-          <canvas id="canvas" width="500" height="250"></canvas>
-  </div>
   <br>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -27,17 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-#mycanvas {
-    width: 100%;
-    max-width: 500px;
-    margin: 40px auto;
-    padding: 28px;
-    border: 1px solid #ffffff;
-    border-radius: 5px;
-    background-color: transparent;
-    box-shadow: 2px 2px 5px 0px rgba(200, 200, 200, 1);
-    font-family: "Times New Roman";
+  margin: auto;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <p>
-      testtext
-    </p>
       <p>count</p><input type="number" max="1000" min="10" :value="top" @change="bottom">
       <br>
-      <div id="app">
-          <canvas id="canvas" width="500" height="250"></canvas>
-      </div>
-      <!--<br>
-      <p>分母</p><input type="number" max="1000" min="10" :value="bottom" @change="top">-->
-      <br>
+      <!--<p>分母</p><input type="number" max="1000" min="10" :value="bottom" @change="top">-->
       <div class="relative">
-        <img id="gazo" class="coffetree" src="https://drive.google.com/uc?export=view&id=1K2j0JCF9CppLWob6YvZYvJR-ROm0NEIJ" width="540" height="960">
-        <img id = "beans1" src="https://drive.google.com/uc?export=view&id=1IR24pMQ6-A1cgiSEhIIR7p5TwPnqr5no" class="absolute">
+        <img id="gazo" src="https://drive.google.com/uc?export=view&id=1K2j0JCF9CppLWob6YvZYvJR-ROm0NEIJ" width="540" height="960">
+        <img id = "beans1" class="beans1" src="https://drive.google.com/uc?export=view&id=1IR24pMQ6-A1cgiSEhIIR7p5TwPnqr5no" >
       </div>
-  </div>
 </template>
 
 <script>
@@ -60,26 +36,13 @@ a {
 }
 .relative {
   position: relative;
+  width:550px;
+  height:970px;
+  margin:auto
 }
-.absolute {
-  position: absolute;
-  right: 1000px;
-  bottom: 1000px;
+.beans1 {
+    position: absolute;
+    top: 150px;
+    left: 150px;
 }
-#app {
-    width: 96%;
-    max-width: 500px;
-    margin: 40px auto;
-    padding: 28px;
-    border: 1px solid #ffffff;
-    border-radius: 5px;
-    background-color: transparent;
-    box-shadow: 2px 2px 5px 0px rgba(200, 200, 200, 1);
-    font-family: "Times New Roman";
-}
-
-#canvas {
-    margin: 0 auto;
-}
-
 </style>
